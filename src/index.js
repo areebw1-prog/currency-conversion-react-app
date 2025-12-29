949b45e6ba0bd7ad549fcec73b56ae0d11b41f2d
areebw1-prog <areebwaqar1@gmail.com>
2025-12-29 06:08:19 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './App.css';
 
+/**
+ * index.js: The entry point of the React application.
+ * It grabs the root element from the HTML and renders the main App component.
+ */
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
